3155c73c7d7886672c8b86a6edea5c88eaa320e0
m45t3r <48780351+m45t3r@users.noreply.github.com>
2020-05-24 12:13:45 -0300

    Use ##__VA_ARGS__ instead of __VA_OPT__

--- src/sdlmenu/sdlmenu.h
+++ src/sdlmenu/sdlmenu.h
@@ -23,8 +23,8 @@ int batt_level(void);
 		memcpy(str, tmp, SBUFFER);                                                                             \
 	} while (0)
 
-// __VA_OPT__ is from C++2a, but it is also available in GNU C++
-#define fmt_disptxt_item(item, format, ...) strfmt(item, format, item __VA_OPT__(, ) __VA_ARGS__)
+// ##__VA_ARGS__ is a GNU C++ extension
+#define fmt_disptxt_item(item, format, ...) strfmt(item, format, item, ##__VA_ARGS__)
 
 #define save_state_fname(fname, ext_format, args...)                                                                   \
 	do {                                                                                                           \
